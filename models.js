266ba87d22eedb97ea72848b8a40693ea16e9aee
MIF20009 <mostafa-fa@outlook.com>
2024-10-07 21:33:40 +0300

    implemented models file

---
+++ models.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const menuItemSchema = new mongoose.Schema({
+    name : {type : String, required : true},
+    price : {type : Number, required : true},
+}, {timestamps : true});
+
+const orderSchema = new mongoose.Schema({
+    item : [{
+        name : {type : String, required : true},
+        quantity : {type : Number, required : true, default : 1},
+    }],
+    status : {
+        type : String,
+        enum : ["Pending", "In Progress", "Completed", "Cancelled"],
+        default : "Pending",
+    },
+},{timestamps : true});
+
+const inventorySchema = new mongoose.Schema({
+    name : {type : String, required : true},
+    quantity : {type : Number, required : true, default : 0},
+}, {timestamps : true});
+
+const reservationSchema = new mongoose.Schema({
+    customername : {type : String, required : true},
+    date : {type : Date, required : true},
+    time : {type : String, required : true},
+    tableNumber : {type : Number, required : true},
+}, {timestamps : true});
+
+const MenuItem = mongoose.model("MenuItem", menuItemSchema);
+const Order = mongoose.model("Order", orderSchema);
+const Inventory = mongoose.model("Inventory",inventorySchema);
+const Reservation = mongoose.model("Reservation", reservationSchema);
+
+module.exports = {MenuItem, Order, Inventory, Reservation};
